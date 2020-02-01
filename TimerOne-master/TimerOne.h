6fbf84bdddd2be279f8db5ed98ca245ff117c5f6
Terry Fairchild <tdfairch2@gmail.com>
2020-02-01 13:37:18 -0600

    Update TimerOne.h

--- TimerOne-master/TimerOne.h
+++ TimerOne-master/TimerOne.h
@@ -8,7 +8,7 @@
  *  Modified July 2017 by Stoyko Dimitrov - added support for ATTiny85 except for the PWM functionality
  *  
  *
- *  This is free software. You can redistribute it and/or modify it under
+ *  This is free software. You can redistribute and/or modify it under
  *  the terms of Creative Commons Attribution 3.0 United States License. 
  *  To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/us/ 
  *  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
